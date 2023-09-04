af73c37bb9913b3736500f42d9fdb08d63bdb50e
sofia <sofiainesschenone@gmail.com>
2023-09-03 22:52:27 -0300

    zoom in & zoom out finished, width buttons

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -5,6 +5,9 @@ import { Map } from 'mapbox-gl';
   styleUrls: ['./zoom-range-page.component.css']
 })
 export class ZoomRangePageComponent implements AfterViewInit {
+
+  public zoom : number = 10;
+  public map?: Map;
   //nos sirve para tomar la referencia local #map en vez de usar un id
   @ViewChild('map') divMap?: ElementRef;
 
@@ -12,11 +15,44 @@ export class ZoomRangePageComponent implements AfterViewInit {
     //hacemos una validación, por si el div es nulo
     if (!this.divMap) throw 'el elemento HTML no fué encontrado';
 
-    const map = new Map({
+   this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+
+    this.mapListeners();
+  }
+
+  mapListeners(){
+    if(!this.map) throw 'Mapa no inicializado';
+    this.map.on('zoom', (event)=> {
+      this.zoom = this.map!.getZoom();
+    })
+
+    //para que no haga demasiado zoom
+    this.map.on('zoomend', (event)=> {
+      if(this.map!.getZoom()> 0) return;
+      this.map!.zoomTo(0);
+      if(this.map!.getZoom()< 18) return;
+      this.map!.zoomTo(18);
+
+    })
+
+  }
+
+  zoomIn(){
+    this.map!.zoomIn();
+  }
+
+  zoomOut(){
+    this.map!.zoomOut();
+  }
+
+  zoomChanged(currentZoom : string){
+    this.zoom = Number(currentZoom);
+    this.map?.zoomTo(this.zoom);
+
   }
 }
