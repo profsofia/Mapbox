f37ff7b847196bec037623172219ad5b5bd36c0e
sofia <sofiainesschenone@gmail.com>
2023-09-07 14:11:03 -0300

    fin de seccion, fin de miniMap componenet

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
 
+  @ViewChild('map') divMap?: ElementRef;
+
+  @Input() lngLat?: [number, number];
+  public zoom: number = 10;
+  public map?: Map;
+
+  ngAfterViewInit(): void {
+     //hacemos una validación, por si el div es nulo
+     if (!this.divMap) throw 'el elemento HTML no fué encontrado';
+     if(!this.lngLat) throw 'LngLat cant be null'
+     const map = new Map({
+       container: this.divMap.nativeElement, // container ID
+       style: 'mapbox://styles/mapbox/streets-v12', // style URL
+       center: this.lngLat, // starting position [lng, lat]
+       zoom: 15, // starting zoom
+       interactive: false
+     });
+
+     new Marker()
+     .setLngLat(this.lngLat)
+     .addTo(map)
+
+  }
 }
