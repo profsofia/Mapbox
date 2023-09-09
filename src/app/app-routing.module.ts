3184f2f1c7055e04b42abbec8464ed27d8a8f2b2
sofia <sofiainesschenone@gmail.com>
2023-09-09 20:37:34 -0300

    Revert "volviendo a un estado anterior"
    This reverts commit 80e01eeabbe50e0a0999a28fb3aed80b6bfd4a58.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'maps',
-    loadChildren: () => import('./maps/maps.module').then((m) => m.MapsModule),
-  },
-  {
-    path: 'alone',
-    loadComponent: () => import('./alone/pages/alone-page/alone-page.component').then((m) => m.AlonePageComponent),
+    path:'maps',
+    loadChildren: () => import('./maps/maps.module').then(m => m.MapsModule),
   },
   {
     path: '**',
-    redirectTo: 'maps',
+    redirectTo: 'maps'
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
